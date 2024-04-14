9a6248c89db08c316cae9ea49e4432d365e1920d
Gibbons <cgibbons@byui.edu>
2024-04-13 20:27:43 -0600

    worked through first two preps

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What are your given names? ");
+        string firsts = Console.ReadLine();
+        Console.Write("What are your family names? ");
+        string lasts = Console.ReadLine();
+        Console.WriteLine($"Your name is: {lasts}, {firsts} {lasts}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?");
+        string enteredGradePc = Console.ReadLine();
+        int gradePc = int.Parse(enteredGradePc);
+        string letterGrade="F";
+        if (gradePc >= 90) {
+            letterGrade = "A";
+        }
+        else if (gradePc >= 80) {
+            letterGrade = "B";
+        }
+        else if (gradePc >= 70) {
+            letterGrade = "C";
+        }
+        else if (gradePc >= 60) {
+            letterGrade = "D";
+        }
+        Console.WriteLine($"Your letter grade will be: {letterGrade}.");
+        if (gradePc >= 70) {
+            Console.WriteLine("That is an acceptable grade.");
+        }
+        else {
+            Console.WriteLine("Please work to get your percentage to 70 or higher.");
+        }
     }
 }

b1c85277334447656113c0cdc6ff1b6a8d6181a2
Carl Gibbons <gibbonsc@byui.edu>
2024-05-01 16:55:14 -0600

    refactored DiceDistribution

--- sandbox/DiceDistribution/DiceDistribution.py
+++ sandbox/DiceDistribution/DiceDistribution.py
@@ -1,26 +1,36 @@
 import random
 
-# initialize a list for counting rolls of dice
-tallies = []
-for i in range(13):
-    tallies.append(0)
-    # the minimum roll of two dice is 2, "snake eyes," so tallies[0] and tallies[1] will not be used
+tallies = []   # for counting the results of dice throws
+experimentSize = 10_000_000   # how many times to throw virtual dice
 
+def prepare_tally_list():
+# initialize the list for counting rolls of dice
+    for i in range(13):
+        tallies.append(0)
+        # the minimum roll of two dice is 2, "snake eyes,"
+        # so tallies[0] and tallies[1] will not be used
+
+def throw_lots_of_dice():
 # roll dice many times and track the results
-experimentSize = 10_000_000
-print("Now rolling ten million pairs of dice.")
-for roll in range(experimentSize):
-    die1 = random.randint(1,6)
-    die2 = random.randint(1,6)
-    total = die1 + die2
-    tallies[total]+=1
+    print(f"Now rolling {experimentSize:,} pairs of dice.")
+    for roll in range(experimentSize):
+        die1 = random.randint(1,6)
+        die2 = random.randint(1,6)
+        total = die1 + die2
+        tallies[total] += 1
+    print(" ... finished rolling. Distribution graph:")
 
-print(" ... finished rolling. Distribution graph:")
+def display_results():
 # display a graph of the tallies; should look somewhat like a triangular "bell curve"
-scale = experimentSize//200
-for i in range(2,13):
-    print(f"{i: 3d}: ", end='')
-    for graph_unit in range(tallies[i]//scale):
-        print("#", end='')
-    print(f" ({tallies[i]:,})")
-print(f" (Scale: each # represents {scale:,} rolls.)")
+    scale = experimentSize//200
+    for i in range(2,13):
+        print(f"{i: 3d}: ", end='')
+        for graph_unit in range(tallies[i]//scale):
+            print("#", end='')
+        print(f" ({tallies[i]:,})")
+    print(f" (Scale: each # represents {scale:,} rolls.)")
+
+# The main program starts here. It just calls the three previously defined funcitons.
+prepare_tally_list()
+throw_lots_of_dice()
+display_results()
--- sandbox/DiceDistribution/Program.cs
+++ sandbox/DiceDistribution/Program.cs
@@ -2,38 +2,57 @@
 
 class Program
 {
-    static void Main(string[] args)
-    { 
-        // initialize a list for counting rolls of dice
-        List<int> tallies = new List<int>();
+    static void PrepareTallyList(List<int> listParam)
+    {
         for (int i=0; i<=12; i++)
         {
-            tallies.Add(0);
+            listParam.Add(0);
         }
         // (The minimum roll of two dice is 2, "snake eyes,"
-        //  so tallies[0] and tallies[1] will not be used)
+        //  so indexes [0] and [1] of this list will not be used)
 
-        // roll dice many times and track the results
-        int experimentSize = 10_000_000;
-        Console.WriteLine("Now rolling ten million pairs of dice...");
+    }
+
+    static void ThrowLotsOfDice(List<int> talliesParam, int quota)
+    {
+        Console.WriteLine($"Now rolling {quota:n0} pairs of dice...");
         Random randomGenerator = new Random();
-        for (int roll=0; roll < experimentSize; roll++)
+        for (int roll=0; roll < quota; roll++)
         {
             int die1 = randomGenerator.Next(1, 7);  // first die roll
             int die2 = randomGenerator.Next(1, 7);  // second die roll
             int total = die1 + die2;
-            tallies[total]++;  // increment the tally for that particular dice sum
+            talliesParam[total]++;  // increment the tally for that particular dice sum
         }
-        
-        Console.WriteLine(" ... finished rolling. Distribution graph: ");
-        // display a graph of the tallies; should look somewhat like a triangular "bell curve"
-        int scale = experimentSize/200;
+        Console.Write(" ... finished rolling. ");
+    }
+
+    static void DisplayResults(List<int> talliesParam, int quota)
+    {
+        int scale = quota/200;
         for (int i=2; i <= 12; i++)
         {
             Console.Write($"{i,3}: ");
-            for (int graphUnit=0; graphUnit < tallies[i]/scale; graphUnit++) { Console.Write("#"); }
-            Console.WriteLine($" ({tallies[i]:n0})");
+            for (int graphUnit=0; graphUnit < talliesParam[i]/scale; graphUnit++)
+            {
+                Console.Write("#");
+            }
+            Console.WriteLine($" ({talliesParam[i]:n0})");
         }
         Console.WriteLine($" (Scale: each # represents {scale:n0} throws of dice.)");
+    }
+
+    static void Main(string[] args)
+    { 
+        // initialize a list for counting rolls of dice
+        List<int> tallies = new List<int>();
+        PrepareTallyList(tallies);
+        
+        // roll dice many times and track the results
+        int experimentSize = 1_000_000_000;
+        ThrowLotsOfDice(tallies, experimentSize);
+
+        // display a graph of the tallies; should look somewhat like a triangular "bell curve"
+        DisplayResults(tallies, experimentSize);
     }
 }

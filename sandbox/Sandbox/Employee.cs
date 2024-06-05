7ee831b946a835d1cba318fe273dea8a08359133
Carl Gibbons <gibbonsc@byui.edu>
2024-06-05 06:48:36 -0600

    Prove05 diagram, sandbox trivia

---
+++ sandbox/Sandbox/Employee.cs
@@ -0,0 +1,20 @@
+public class Employee
+{
+    private float salary = 100f;
+
+    public virtual float CalculatePay()
+    {
+        return salary;
+    }
+}
+
+public class HourlyEmployee : Employee
+{
+    private float rate=9f;
+    private float hours=100f;
+
+    public override float CalculatePay()
+    {
+        return rate*hours;
+    }
+}
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,11 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Course course1 = new Course();
-        course1._className = "Prog. w/Classes";
-        course1._color = "green";
-        course1._courseCode = "CSE 201";
-        course1._numberOfCredits = 2;
-        course1.Display();
+        Employee e = new Employee();
+        Console.WriteLine($"{e.CalculatePay()}");
+        Employee ee = new HourlyEmployee();
+        Console.WriteLine($"{ee.CalculatePay()}");
     }
 }

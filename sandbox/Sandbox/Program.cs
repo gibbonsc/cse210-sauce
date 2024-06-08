15689dfc7e4ff3846d6da26bf189a9cc8bb09cc3
Carl Gibbons <gibbonsc@byui.edu>
2024-06-08 14:16:09 -0600

    reset Sandbox greeting

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,9 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Employee e = new Employee();
-        Console.WriteLine($"{e.CalculatePay()}");
-        Employee ee = new HourlyEmployee();
-        Console.WriteLine($"{ee.CalculatePay()}");
+        string greeting = "Howdy Sandbox World!";
+        Console.WriteLine($"{greeting}");
     }
 }

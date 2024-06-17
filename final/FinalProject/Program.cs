6c2fcffac1502376bdcddd0a1011c33517f02822
Carl Gibbons <gibbonsc@byui.edu>
2024-06-17 11:06:01 -0600

    cleaned up sandboxes, updated final diagrams

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using Windows.Forms;
 
 class Program
 {
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,9 @@
-using System;
+﻿namespace OnlineOrdering;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        Console.WriteLine("Hello, Foundation2 World!");
     }
 }
